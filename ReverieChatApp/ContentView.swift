7f80a5b601597f4f503559c32fd2b20037a7e38e
Anatolii <anatolii.semenchuk@schindler.com>
2026-04-20 09:55:50 +0300

    Initial Commit

---
+++ ReverieChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReverieChatApp
+//
+//  Created by Anatolii Semenchuk on 20.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReverieChatApp/ReverieChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverieChatAppApp.swift
+//  ReverieChatApp
+//
+//  Created by Anatolii Semenchuk on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReverieChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
